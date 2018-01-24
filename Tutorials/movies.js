86173dc7e1705cebe4cf0a2abb1aebfaa6d27653
NextiaDev <arrodl40@gmail.com>
2018-01-24 14:57:09 -0600

    movies importing modules

---
+++ Tutorials/movies.js
@@ -0,0 +1,10 @@
+function printAvatar () {
+	console.log("Avatar: PG-13");
+}
+
+function pringChappie () {
+	console.log("Chappie: R");
+}
+
+// Determines which code is to be exported
+module.exports.avatar = printAvatar;
